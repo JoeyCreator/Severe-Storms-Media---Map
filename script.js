35a742d6fa6df3e0802de14499a38b9b01cb1aa6
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-03 18:51:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const mapbox_token = "pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2trcG14bGJtMnltcTJvbndudThkb2drZiJ9.WjTov6fnMyLClhzWYq8Fig"
+
+mapboxgl.accessToken = mapbox_token;
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/dark-v10',
+  center: [-96.052335, 39.159882],
+  zoom: 3.5
+});
+
+
+// create the popup
+
+
+
+// GeoLocation **
+
+var nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-left');
+
+//End*

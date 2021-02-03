47ff68d7c148812fc7c30e618de6922ef40788a0
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-03 23:25:10 +0000

    bug

--- script.js
+++ script.js
@@ -6,40 +6,10 @@ var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/dark-v10',
   center: [-73.5673, 45.5017],
-  zoom: 8.5
+  zoom: 3.5
 });
 
 var nav = new mapboxgl.NavigationControl();
 map.addControl(nav, 'top-left');
 
 // edit code below this comment //
-
-map.on('load', function () {
-  map.addSource('Montreal', {
-    'type': 'geojson',
-    'data': {
-      'type': 'Feature',
-      'geometry': {
-        'type': 'Polygon',
-        'coordinates': [
-          [
-            [-73.5673, 45.5017],
-            [-73.8206, 45.4896],
-            [-73.7500, 45.4503],
-            [-73.7500, 45.4503],
-          ]
-        ]
-      }
-    }
-  });
-  map.addLayer({
-    'id': 'Montreal',
-    'type': 'fill',
-    'source': 'Montreal',
-    'layout': {},
-    'paint': {
-      'fill-color': 'red',
-      'fill-opacity': 0.5
-    }
-  });
-});

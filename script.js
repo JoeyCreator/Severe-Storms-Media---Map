f6e714e6e0a79b9f3b618d4924e0a1c1021337ec
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-04 21:16:15 +0000

    modify map.addsource

--- script.js
+++ script.js
@@ -18,31 +18,31 @@ map.scrollZoom.disable();
 // Storm Prediction Center // => add code below this comment //
 
 map.on('load', function () {
-var layers = map.getStyle().layers;
-// Find the index of the first symbol layer in the map style
-var firstSymbolId;
-for (var i = 0; i < layers.length; i++) {
-if (layers[i].type === 'symbol') {
-firstSymbolId = layers[i].id;
-break;
-}
-}
-map.addSource('urban-areas', {
-'type': 'geojson',
-'data':
-'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
-});
-map.addLayer(
-{
-'id': 'urban-areas-fill',
-'type': 'fill',
-'source': 'urban-areas',
-'layout': {},
-'paint': {
-'fill-color': '#f08',
-'fill-opacity': 0.4
-}
-},
-firstSymbolId
-);
+  var layers = map.getStyle().layers;
+  // Find the index of the first symbol layer in the map style
+  var firstSymbolId;
+  for (var i = 0; i < layers.length; i++) {
+    if (layers[i].type === 'symbol') {
+      firstSymbolId = layers[i].id;
+      break;
+    }
+  }
+  map.addSource('spcday1', {
+    'type': 'geojson',
+    'data':
+      'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
+  });
+  map.addLayer(
+    {
+      'id': 'spcday1',
+      'type': 'fill',
+      'source': 'spcday1',
+      'layout': {},
+      'paint': {
+        'fill-color': '#FF9A00',
+        'fill-opacity': 0.4
+      }
+    },
+    firstSymbolId
+  );
 });

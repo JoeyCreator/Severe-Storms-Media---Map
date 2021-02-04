905f5bc02c27a9dd8baa9c3351a2f8dfff6f7862
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-04 02:38:54 +0000

    Add control

--- script.js
+++ script.js
@@ -9,19 +9,9 @@ var map = new mapboxgl.Map({
   zoom: 3.5
 });
 
-// Edit Code Below This Comment //
 
-map.addSource('spcnoaa', {
-  type: 'geojson',
-  data: 'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
-});
+var nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-left');
 
-let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson');
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.responseType = 'json';
-xhr.onload = function () {
-  if (xhr.status !== 200) return
-  L.geoJSON(xhr.response).addTo(map);
-};
-xhr.send();
+
+// Add Storm Prediction Center Outloo //*

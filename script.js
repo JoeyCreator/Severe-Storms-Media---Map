cdb4a4519aa4c5ee5792dcfaa2991b74a2d531dc
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-04 00:52:52 +0000

    get

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ var map = new mapboxgl.Map({
 
 // Edit Code Below This Comment //
 
+map.addSource('spcnoaa', {
+  type: 'geojson',
+  data: 'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
+});
+
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson');
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.responseType = 'json';
+xhr.onload = function () {
+  if (xhr.status !== 200) return
+  L.geoJSON(xhr.response).addTo(map);
+};
+xhr.send();

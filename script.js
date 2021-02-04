9ea3d65cace5598116cffe37b0342230f00b477c
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-04 19:59:43 +0000

    dev ops

--- script.js
+++ script.js
@@ -15,9 +15,41 @@ map.addControl(nav, 'top-left');
 // disable map zoom when using scroll
 map.scrollZoom.disable();
 
-// Storm Prediction Center //
+// Storm Prediction Center // => add code below this comment //
 
-map.addSource('nooaspc', {
-  type: 'geojson',
-  data: 'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
+map.on('load', function () {
+var layers = map.getStyle().layers;
+// Find the index of the first symbol layer in the map style
+var firstSymbolId;
+for (var i = 0; i < layers.length; i++) {
+if (layers[i].type === 'symbol') {
+firstSymbolId = layers[i].id;
+break;
+}
+}
+map.addSource('urban-areas', {
+'type': 'geojson',
+'data':
+'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
+});
+map.addLayer(
+{
+'id': 'urban-areas-fill',
+'type': 'fill',
+'source': 'urban-areas',
+'layout': {},
+'paint': {
+'fill-color': '#f08',
+'fill-opacity': 0.4
+}
+// This is the important part of this example: the addLayer
+// method takes 2 arguments: the layer as an object, and a string
+// representing another layer's name. if the other layer
+// exists in the stylesheet already, the new layer will be positioned
+// right before that layer in the stack, making it possible to put
+// 'overlays' anywhere in the layer stack.
+// Insert the layer beneath the first symbol layer.
+},
+firstSymbolId
+);
 });

3b85350d28e4b9fd150e9e868ba01f60427765b3
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-05 16:24:36 +0000

    Updating spcday1

--- script.js
+++ script.js
@@ -18,39 +18,31 @@ map.scrollZoom.disable();
 // Storm Prediction Center -> Spcd Day 1 //
 
 map.on('load', function () {
-var layers = map.getStyle().layers;
-// Find the index of the first symbol layer in the map style
-var firstSymbolId;
-for (var i = 0; i < layers.length; i++) {
-if (layers[i].type === 'symbol') {
-firstSymbolId = layers[i].id;
-break;
-}
-}
-map.addSource('urban-areas', {
-'type': 'geojson',
-'data':
-'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
-});
-map.addLayer(
-{
-'id': 'urban-areas-fill',
-'type': 'fill',
-'source': 'urban-areas',
-'layout': {},
-'paint': {
-'fill-color': ['get', 'fill'],
-'fill-outline-color': ['get', 'stroke'],
-'fill-opacity': 0.5
-}
-// This is the important part of this example: the addLayer
-// method takes 2 arguments: the layer as an object, and a string
-// representing another layer's name. if the other layer
-// exists in the stylesheet already, the new layer will be positioned
-// right before that layer in the stack, making it possible to put
-// 'overlays' anywhere in the layer stack.
-// Insert the layer beneath the first symbol layer.
-},
-firstSymbolId
-);
+  var layers = map.getStyle().layers;
+  var firstSymbolId;
+  for (var i = 0; i < layers.length; i++) {
+    if (layers[i].type === 'symbol') {
+      firstSymbolId = layers[i].id;
+      break;
+    }
+  }
+  map.addSource('spcday1', {
+    'type': 'geojson',
+    'data':
+      'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
+  });
+  map.addLayer(
+    {
+      'id': 'spcday1',
+      'type': 'fill',
+      'source': 'spcday1',
+      'layout': {},
+      'paint': {
+        'fill-color': ['get', 'fill'],
+        'fill-outline-color': ['get', 'stroke'],
+        'fill-opacity': 0.5
+      }
+    },
+    firstSymbolId
+  );
 });

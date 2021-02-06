e3091736c31f531ed3154b9555a2a00b22981cff
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-06 13:16:21 +0000

    Zoom 3.5

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v10',
   center: [-96.052335, 39.159882],
-  zoom: 4.5
+  zoom: 3.5
 });
 
 // disable map zoom when using scroll

93d888c21934f36ed83f709b530169beab740b22
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-02-23 15:16:40 +0000

    deploy to netlify

--- googlechrome/devtools/console/consolemsg.js
+++ googlechrome/devtools/console/consolemsg.js
@@ -1,6 +1,6 @@
 var cssRule =
     "color: red;" +
-    "-webkit-text-stroke: 4px black;" + 
+    "-webkit-text-stroke: 4px black;" +
     "font-size: 60px;" +
     "font-weight: bold;" +
     "text-shadow: 1px 1px 5px rgb(249, 162, 34);" +
@@ -14,3 +14,12 @@ var cssRule =
     "text-shadow: 1px 1px 5px rgb(249, 162, 34);" +
     "filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);";
 console.log("%cThis is a browser feature intended for developers.", cssRule);
+
+
+var cssRule =
+    "color:" +
+    "font-size:" +
+    "font-weight:" +
+    "text-shadow:" +
+    "filter:" +
+    console.log("%c", cssRule);

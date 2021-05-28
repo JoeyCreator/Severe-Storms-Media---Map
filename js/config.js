558cca79587b0e3c3d6eaf17242f9f6e71d5525f
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-05-28 22:17:00 +0000

    hello div

--- js/config.js
+++ js/config.js
@@ -5,18 +5,14 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     maxZoom: 4.1,
     id: 'mapbox/dark-v10',
     tileSize: 512,
-     zoomOffset: -1,
+    zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2tva253dHRxMDFhMzJubzF0NmNidTV1byJ9.fuQQQ11Nb0tnr-jbWemOsQ'
 }).addTo(mymap);
 
+var subs = ["mesonet", "mesonet1", "mesonet2", "mesonet3"];
 var nexrad = L.tileLayer.wms("https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0q.cgi", {
     layers: 'nexrad-n0r-900913',
     format: 'image/png',
     transparent: false,
     attribution: "Weather data © 2012 IEM Nexrad"
-});
-
-
-var map = L.map(mapDiv, mapOptions);
-
-var wmsLayer = L.tileLayer.wms('http://mesonet.agron.iastate.edu/geojson/nexrad_attr.geojson?callback=gotData', wmsOptions).addTo(map);
+}).addTo(mymap);

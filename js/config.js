d69aa5f8d92b844ef0bd58f359a9df13901c4816
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-05-29 15:24:43 +0000

    INC

--- js/config.js
+++ js/config.js
@@ -10,9 +10,13 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 var subs = ["mesonet", "mesonet1", "mesonet2", "mesonet3"];
-var nexrad = L.tileLayer.wms("https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0q.cgi", {
-    layers: 'n0q',
+var nexrad = L.tileLayer.wms("https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0q.cgi?", {
+    layers: 'nexrad-n0q-900913',
     format: 'image/png',
     transparent: true,
     attribution: "Weather data © 2012 IEM Nexrad"
-}).addTo(mymap);
+});
+
+var wmsLayer = L.tileLayer.wms('http://mesonet.agron.iastate.edu/cache/tile.py/1.0.0/nexrad-n0q-900913/', {
+    layers: 'TOPO-OSM-WMS'
+}).addTo(map);

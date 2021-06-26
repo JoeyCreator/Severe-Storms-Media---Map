a43b75ceb79eb9685633d73179e18ed31298dd0e
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-06-26 23:35:58 +0000

    adding spc noaa

--- js/config.js
+++ js/config.js
@@ -1,12 +1,31 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2tva253dHRxMDFhMzJubzF0NmNidTV1byJ9.fuQQQ11Nb0tnr-jbWemOsQ';
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2txZWU1eHpiMDBuczJwcjFzYzgyaXlmaSJ9.Zuq7AJyK1RRGN-ZFbJxY6A';
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/dark-v10',
-  center: [-96.052335, 39.159882]
+  center: [-96.052335, 39.159882],
   zoom: 4.5
 });
 
-map.addSource('socday1', {
+map.scrollZoom.disable();
+
+map.addSource('spcday1', {
   type: 'geojson',
   data: 'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
+});
+
+
+
+map.addSource('spcday1', {
+  type: 'geojson',
+  data: {
+    "type": "FeatureCollection",
+    "features": [{
+      "type": "Feature",
+      'paint': {
+        'fill-color': ['get', 'fill'],
+        'fill-outline-color': ['get', 'stroke'],
+        'fill-opacity': 0.5
+      }
+    }]
+  }
 });

4922c935907b1a5ee3495f8d15566cdc242bcc1a
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-08-31 22:06:54 +0000

    inc

--- js/config.js
+++ js/config.js
@@ -1,6 +1,24 @@
-var map = new maplibregl.Map({
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9leWNyZWF0b3IiLCJhIjoiY2tzM214YnU1MHRuMTJwcnd1OWVocW5hNyJ9.WVXk8pjKg6KMAOYdL8nDRg';
+var map = new mapboxgl.Map({
 	container: 'map',
-	style: 'https://api.maptiler.com/maps/lights/style.json?key=<Vcy11PZ7JhCsbf4TZmOO>',
-	center: [-96.052335, 39.159882],
-	zoom: 4.5
+	center: [-95.7129, 37.0902],
+	zoom: 4.3,
+	style: 'mapbox://styles/mapbox/dark-v10'
+});
+
+
+map.addSource('spcday1', {
+	type: 'geojson',
+	data: 'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
+});
+
+map.getSource('spcday1').setData({
+	'id': 'spcday1',
+	'type': 'fill',
+	'source': 'spcday1',
+	'paint': {
+		'fill-color': ['get', 'fill'],
+		'fill-outline-color': ['get', 'stroke'],
+		'fill-opacity': 0.5
+	}
 });

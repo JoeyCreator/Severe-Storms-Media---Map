a3b24d1a1f533660d9497a7f82d430c5f91d42ac
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-09-05 19:36:52 +0000

    adding geojson data

--- js/config.js
+++ js/config.js
@@ -6,8 +6,25 @@ var map = new mapboxgl.Map({
 	style: 'mapbox://styles/mapbox/dark-v10'
 });
 
+// disable map zoom when using scroll
+map.scrollZoom.disable();
 
-map.addSource('spcday1', {
-	type: 'geojson',
-	data: 'https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
+
+map.on('load', () => {
+	map.addSource('spcday1', {
+		type: 'geojson',
+		// Use a URL for the value for the `data` property.
+		data: ' https://www.spc.noaa.gov/products/outlook/day1otlk_cat.nolyr.geojson'
+	});
+
+	map.addLayer({
+		'id': 'spcday1-layer',
+		'type': 'fill',
+		'source': 'spcday1',
+		'paint': {
+			'fill-color': ['get', 'fill'],
+			'fill-outline-color': ['get', 'stroke'],
+			'fill-opacity': 0.5
+		}
+	});
 });
